bd27273a8e4c425c714d6bc54e3804702be0bf2a
Earl Lauriece S. Butlay <earl0bluesky142226@gmail.com>
2025-03-19 15:00:03 +0800

    2nd commit -- upload topics and messages

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+For this project, you will build a message generator program. Every time a user runs a program, 
+they should get a new, randomized output. You’re welcome to take the project in a couple of different forms, 
+like an astrology generator, inspirational message, or nonsensical jokes. To make your program truly random, 
+the message that it outputs should be made up of at least three different pieces of data. 
+Take what you know of JavaScript syntax so far to build the program and customize it to your liking.
+
+Project Objectives:
+
+    Build a message generator program using JavaScript
+    Use Git version control
+    Use command line
+    Develop locally on your computer
+
+Prerequisites:
+
+    JavaScript
+    Git and GitHub
+    Command line
+
+*/
+
+// Array of Motivational Quotes
+const quotes = [
+    "Believe you can and you're halfway there.",
+    "Success is not final, failure is not fatal.",
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "Dream big and dare to fail.",
+    "Start where you are. Use what you have. Do what you can."
+  ];
+  
+  // Array of Bible Verses
+  const bibleVerses = [
+    "Philippians 4:13 - I can do all things through Christ who strengthens me.",
+    "Jeremiah 29:11 - For I know the plans I have for you, declares the Lord.",
+    "Proverbs 3:5-6 - Trust in the Lord with all your heart and lean not on your own understanding.",
+    "Psalm 23:1 - The Lord is my shepherd; I shall not want.",
+    "Isaiah 41:10 - Fear not, for I am with you; be not dismayed, for I am your God."
+  ];
+  
+  // Array of Life Tips/Reminders
+  const lifeTips = [
+    "Stay consistent and keep working towards your goals.",
+    "Take breaks to avoid burnout and stay productive.",
+    "Be kind to yourself and others.",
+    "Learn something new every day.",
+    "Practice gratitude and stay positive."
+  ];
+  
